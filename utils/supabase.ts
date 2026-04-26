98d2855277ab3999fe700f469cded8efc740c250
Joshua Hartshorne <jnhart@DESKTOP-59EOCHQ.localdomain>
2026-04-26 14:39:51 +0100

    Persist auth

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,18 +1,25 @@
+import type { Database } from '@/types/database.types';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AppState, Platform } from 'react-native';
 import { createClient, processLock } from '@supabase/supabase-js';
-import type { Database } from '@/types/database.types';
 
 const supabaseUrl = 'https://aqcmjdnqncecnwmvlbld.supabase.co';
 const supabasePublishableKey = 'sb_publishable_JyFPgdAvItagcaUBwqVm3A_jAScd4NI';
 
-export const supabase = createClient<Database>(supabaseUrl, supabasePublishableKey, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-    lock: processLock,
+export const supabase = createClient<Database>(
+  supabaseUrl,
+  supabasePublishableKey,
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+      lock: processLock,
+    },
   },
-});
+);
 
 if (Platform.OS !== 'web') {
   AppState.addEventListener('change', (state) => {
